3b32fb1d067ee8be504dad6a84d71f09479b4294
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-06-10 21:04:27 +0530

    fixed json types

--- types/user.go
+++ types/user.go
@@ -3,16 +3,16 @@ package user
 import "encoding/json"
 
 type Address struct {
-	City    string
-	State   string
-	Country string
-	Pincode json.Number
+	City    string      `json:"addressCity"`
+	State   string      `json:"addressState"`
+	Country string      `json:"addressCountry"`
+	Pincode json.Number `json:"addressPincode"`
 }
 
 type User struct {
-	Name    string
-	Age     json.Number
-	Contact string
-	Company string
-	Address Address
+	Name    string      `json:"userName"`
+	Age     json.Number `json:"userAge"`
+	Contact string      `json:"userContact"`
+	Company string      `json:"userCompany"`
+	Address Address     `json:"userAddress"`
 }
