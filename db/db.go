846428309b28b23d07186ebc6ced668b4d0bf317
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-06-10 14:25:23 +0530

    db write function added

--- db/db.go
+++ db/db.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sync"
@@ -53,7 +56,36 @@ func New(dir string, options *Options) (*Driver, error) {
 
 }
 
-func (d *Driver) Write() error {
+func (d *Driver) Write(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save record!")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - no place to save record!")
+	}
+
+	mutex := d.getOrCreateMutex()
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, collection)
+	fnlPath := filepath.Join(dir, resource+".json")
+	tmpPath := fnlPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("Failed to create directory %s: %w", dir, err)
+	}
+
+	b, err := json.MarshalIndent(v, "", " \t")
+	if err != nil {
+		return err
+	}
+
+	b = append(b, byte('\n'))
+	if err := ioutil.WriteFile(tmpPath, b, 0644); err != nil {
+		return fmt.Errorf("Failed to write to temporary file %s: %w", tmpPath, err)
+	}
 
 }
 
@@ -66,6 +98,13 @@ func (d *Driver) Read() ([]byte, error) {
 func (d *Driver) Delete() error {
 }
 
-func (d *Driver) getOrCreateMutex() {
+func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
 
+}
+
+func stat(path string) (fi os.FileInfo, err error) {
+	if fi, err = os.Stat(path); os.IsNotExist(err) {
+		fi, err = os.Stat(path + ".json")
+	}
+	return
 }
